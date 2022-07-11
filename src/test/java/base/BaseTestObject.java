562eedc91a9f597b4be52fa2763aaba43025b220
chethanalb <108932412+chethanalb@users.noreply.github.com>
2022-07-11 19:34:56 +0530

    Update BaseTestObject.java

--- src/test/java/base/BaseTestObject.java
+++ src/test/java/base/BaseTestObject.java
@@ -40,8 +40,8 @@ public class BaseTestObject {
 
             WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
- //           options.addArguments("--headless");
             options.addArguments("--no-sandbox");
+            options.addArguments("--headless");
             options.addArguments("--window-size=1920,1080");
             options.addArguments("--start-maximized");
             driver = new ChromeDriver(options);
